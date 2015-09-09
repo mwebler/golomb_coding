93773f9c3ffedbd39cf6c03ca52d4c12c462251c
Matheus Webler <matheus.webler@gmail.com>
2015-09-09 00:14:07 +0000

    read file and show text and hexadecimal codes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,28 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+function handleFileSelect(evt) {
+  var f = evt.target.files[0]; // FileList object
+  var reader = new FileReader();
+
+  // Closure to capture the file information.
+  reader.onload = (function(theFile) {
+    return function(e) {
+      var markup, result, n, aByte, byteStr;
+      var res = e.target.result;
+      var str = [];
+      for (n = 0; n < res.length; ++n) {
+        aByte = res.charCodeAt(n);
+        byteStr = aByte.toString(16).toUpperCase();
+        if (byteStr.length < 2) {
+          byteStr = "0" + byteStr;
+        }
+        str.push(byteStr);
+      }
+
+      $('#texto')[0].innerHTML = res + "<br>" + str;
+    };
+  })(f);
+
+  reader.readAsBinaryString(f);
+}
+
+$('#files')[0].addEventListener('change', handleFileSelect, false);
