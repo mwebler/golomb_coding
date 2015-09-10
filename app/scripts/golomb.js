58b83d86aa4f8c72d7abdbe8d411b5f41f9f1f47
Matheus Webler <matheus.webler@gmail.com>
2015-09-10 04:26:51 +0000

    finished encoder algorithm

--- app/scripts/golomb.js
+++ app/scripts/golomb.js
@@ -1,18 +1,52 @@
 var Golombzr = function (config) {
     
     var options = {
-        'divisor': config.divisor || 8,
-        'stopbit': config.stopbit || 0 
+        'divisor':          config.divisor || 8,
+        'stopbit':          config.stopbit || 1
     };
     
-    var toUnary = function(value){
-        return Array(value+1).join(options.stopbit);
+    var divisor_base = Math.log2(options.divisor);
+    var remainder_size = Math.ceil(divisor_base);
+    var isRice = divisor_base == remainder_size;
+    
+        
+    var getQuotient = function (value){
+        if(isRice){
+            return value >> divisor_base;
+        }
+        else{
+           return Math.floor(value/options.divisor);
+        }
+    };
+    
+    var getRemainder = function (value){
+        if(isRice){
+            return value & (options.divisor-1);
+        }
+        else{
+           return value % options.divisor
+        }
+    };
+    
+    var getUnaryPortion = function(quotient){
+        var bit = options.stopbit == 0 ? '1' : '0';
+        return Array(quotient+1).join(bit);
+    };
+    
+    var getBinaryPortion = function(remainder){
+        var binary = remainder.toString(2);
+        if(binary.length >= remainder_size){
+            return binary;
+        }   
+        else{
+            return Array(remainder_size + 1 - binary.length).join('0') + binary;
+        }
     };
     
     var encode = function(value) {
-        var quotient = Math.floor(value/options.divisor); 
-        var remainder = value%options.divisor;
-        var code = toUnary(quotient) + ' ' + (~quotient >>> 0).toString(2);
+        var quotient = getQuotient(value);
+        var remainder = getRemainder(value);
+        var code = getUnaryPortion(quotient) + options.stopbit + getBinaryPortion(remainder);
         return {
             'value': value,
             'quotient': quotient,
